6c05c665209fddced0c7264701597768f9879f0e
Bergschaf <christian.k@netcom-mail.de>
2022-09-23 19:13:42 +0200

    cpp innit

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //header file library 
+using namespace std; //using standard library
+
+int main() { //main function
+    cout << "Hello World \n"; // first object
+    cout << "Learn C++ \n\n"; //second object with blank line
+    cout << "Educative Team"; //third object
+    return 0; //no other output or return
+} //end of code to exectute
