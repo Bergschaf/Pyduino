9216039770dbc971985d91194ca9796968f8a6fd
Bergschaf <christian.k@netcom-mail.de>
2022-10-03 14:26:21 +0200

    Compiler: builtins functions innit

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
+from builtins import check_builtin
 
-
-def do_function(row_index, line):
+def do_function_definition(row_index, line):
     pass
 
 
-def check_builtin(row_index, line):
-    pass
+def do_function_execution(row_index, line):
+    builtin = check_builtin(line)
+
+
