6b11c4cfc62b3203728f80b8fc358f8ee5dd1aba
bergschaf <christian.k@netcpm-mail.de>
2023-03-05 02:52:49 +0100

    LAAAANGFASSUNG

--- run.py
+++ run.py
@@ -3,8 +3,10 @@ import subprocess
 from server.transpiler.runner import Runner
 
 if __name__ == '__main__':
-    file = sys.argv[1]
-    #file = "test.pino"
+    if len(sys.argv) < 2:
+        file = "test.pino"
+    else:
+        file = sys.argv[1]
 
     with open(file, "r") as f:
         runner = Runner(f.read())
